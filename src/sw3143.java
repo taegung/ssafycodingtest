b4fa8b5c995b2cd98e8c71e74795449d79cf4e45
taegung <sungminyun12@daum.net>
2024-11-07 18:16:46 +0900

    sw3143

--- src/sw3143.java
+++ src/sw3143.java
@@ -2,7 +2,16 @@ import java.util.*;
 public class sw3143 {
     public static void main(String[] args){
 
+        Scanner in =new Scanner(System.in);
+        int T = in.nextInt();
+        for(int i=1;i<T+1;i++) {
+            String A = in.next();
+            String B = in.next();
+            A = A.replace(B, " ");
+            int lenth = A.length();
+            System.out.println("#"+i+" "+lenth);
 
+        }
     }
 
 }
